7b8d72685f8cbbf0b9f6080fd9994536845c8fff
Ramon Thomaz <ramon.thomaz@agapys.com>
2023-09-11 12:37:18 +0100

    Feat: INativeActionDriver interface
    An interface for a class that shall be responsible for managing the actions performed on java elements.

---
+++ src/JavaAutoNet.Core/Actions/NativeActions/INativeActionDriver.cs
@@ -0,0 +1,27 @@
+﻿using JavaAutoNet.Core.Enums.NativeActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaAutoNet.Core.Actions.NativeActions
+{
+    public interface INativeActionDriver
+    {
+        /// <summary>
+        /// Returns an IEumerable with all the possible actions for a given java element.
+        /// </summary>
+        /// <param name="vmID">The vm's id.</param>
+        /// <param name="referenceJavaObjHandle">The reference java object's native handle.</param>
+        /// <returns></returns>
+        IEnumerable<NativeAction> GetPossibleNativeActions(int vmID, IntPtr referenceJavaObjHandle);
+        /// <summary>
+        /// Executes a given native action on the element.
+        /// </summary>
+        /// <param name="vmID">The vm's id.</param>
+        /// <param name="referenceJavaObjHandle">The reference java object's native handle.</param>
+        /// <param name="nativeAction">The desired native action to be performed.</param>
+        void DoNativeAction(int vmID, IntPtr referenceJavaObjHandle, NativeAction nativeAction);
+    }
+}
